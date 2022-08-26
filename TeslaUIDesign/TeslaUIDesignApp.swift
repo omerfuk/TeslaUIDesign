42c555e60260933b690d7e09e08019472d6bfbcc
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-08-26 09:49:13 +0300

    Initial Commit

---
+++ TeslaUIDesign/TeslaUIDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaUIDesignApp.swift
+//  TeslaUIDesign
+//
+//  Created by Ömer Faruk Kılıçaslan on 26.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TeslaUIDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
